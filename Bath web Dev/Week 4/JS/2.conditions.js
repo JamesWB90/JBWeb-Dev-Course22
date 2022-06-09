bc21a0fed3ce80c0cd5f71ab00bce19e7bba0f07
JamesWB90 <baileyjames48@gmail.com>
2022-06-09 17:02:37 +0100

    update 9/6

---
+++ Bath web Dev/Week 4/JS/2.conditions.js
@@ -0,0 +1,85 @@
+//console.log("hello James")
+
+
+//Suedo Coding
+
+// Going out 
+// take Texi
+// Enter airport
+// Checkin
+
+//if raining take rain coat
+//else dont take rain coat
+
+//var tomr='rain';
+//if(tomr=='rain'){
+//    console.log('take a raincoat');
+//}
+
+//else{
+
+//  console.log('no need for raincoat');
+//}
+
+// write a program to check leap year
+
+// let year = 2024;
+// debugger
+
+// if (year % 4 === 0) {
+
+//     if (year % 100 !== 0) {
+
+//         if (year % 400 === 0) {
+//             console.log("The year" + year + " is a Leap Year!")
+//         } else {
+//             console.log("The year" + year + " is not a Leap Year...")
+//         }
+//     } else {
+//         console.log("The year" + year + " is not a Leap Year...")
+//     }
+// } else {
+//     console.log("The year" + year + " is not a Leap Year...")
+// };
+
+//
+// var age=17;
+// if(age>=18){
+// console.log("You are eligible to vote");
+// }
+// else{
+// console.log("You are not eligible to vote");
+// }
+
+// var age=17;
+// console.log((age>=18)?"you can vote":"you can't");
+
+// var area="square";
+// var PI=3.142,l=5,b=4,r=3;
+// debugger
+// if(area==="circle"){
+//     console.log("the area of the circle is:"+PI*r**2);
+//     }else if(area="triangle"){
+//     console.log("the area of the triangle is:"+(l+b)/2);
+//     }else if(area="rectangle"){
+//     console.log("the area of the rectangle is:"+(l*b));
+//     }else{
+//     console.log("please enter valid data");
+//     }
+
+// var area="triangle";
+// var PI=3.142,l=5,b=4,r=3;
+
+
+// switch(area){
+//     case 'circle': console.log("the area of the circle is"+PI*r**2);
+//     case 'triange': console.log("the area of the triangle is"+PI*r**2);
+
+
+
+
+// }
+
+//8*1=8
+
+
