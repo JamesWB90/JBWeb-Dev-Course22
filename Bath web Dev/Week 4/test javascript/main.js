f9790445d110b95a06583c7feb1a4c0e99a98aae
JamesWB90 <baileyjames48@gmail.com>
2022-06-16 14:15:09 +0100

    update 16/06

---
+++ Bath web Dev/Week 4/test javascript/main.js
@@ -0,0 +1,15 @@
+// veruiables- 
+//var
+//let - (you can re assign values),
+//const - constant value and cant be changed or re assigned
+
+
+//data types
+
+//Strings - this is words or multiple caracters 
+// numbers 
+const name = "James"; // String
+ const age = 31; // number
+const iscool = true //buolean -
+null -
+undifined
