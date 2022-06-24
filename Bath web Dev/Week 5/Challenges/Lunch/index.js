8a7b692be4edf200e9e8f4c582f505d37b43bd4a
JamesWB90 <baileyjames48@gmail.com>
2022-06-24 15:55:20 +0100

    update

---
+++ Bath web Dev/Week 5/Challenges/Lunch/index.js
@@ -0,0 +1,7 @@
+const names  = ['James' , 'Adam' , 'Luke' , 'Cherish' , 'Leroy' , 'Isaiah']
+const random = names[Math.floor(Math.random() * names.length)];
+console.log(random + ' is going to buy lunch today!');
+
+function create_random_string(){
+    let = ('James', 'Adam' , 'Luke' , 'Cherish' , 'Leroy' , 'Isaiah')
+}
